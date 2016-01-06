96471b32d9f4c24baeb879171e23c2b5288a577f
burcukaraman <burcukaramanlioglu@gmail.com>
2016-01-06 03:00:09 +0200

    az kaldı

---
+++ database.js
@@ -0,0 +1,49 @@
+var catalog = [
+	{
+        "itemName":"Martı",
+        "itemAuthor":"Jonathan Livingston",
+        "itemImage":"image/image1.png",
+        "itemCreationDate":"1995",
+        "itemDescription":"<p>Durgun denizin minik dalgacıkları üzerinde, güneşin altın gibi ışıldadığı pırıl pırıl bir sabahtı Sahilden bir mil uzaklıkta, denizi kucaklarcasına ilerleyen bir balıkçı teknesi, martılara kahvaltı zamanının geldiğini haber veriyordu. Binlerce martı, bir lokma yiyecek için mücadeleye girişmişti bile. İşte zor bir gün daha başlıyordu!</p>"
+    }, 
+	{
+        "itemName":"Pembe ve Yusuf",
+        "itemAuthor":"Canan Tan",
+        "itemImage":"image/image2.jpg",
+        "itemCreationDate":"2014",
+        "itemDescription":"<p>Birbirlerine delicesine düşkün iki kardeşin,Pembe ile Yusuf’un sızılı ve çarpıcı öyküsü Ezenler ve ezilenlerin amansız savaşını, üzerinde yaşadığımız coğrafyanın değişmez kaderi anlatan bir kitap</p>"
+    }, 
+	{
+        "itemName":"Ya Sen Olmasaydın",
+        "itemAuthor":"İpek Ongun",
+        "itemImage":"image/image3.png",
+        "itemCreationDate":"2010",
+        "itemDescription":"<p>Ya Sen Olmasaydın / Bir Genç Kızın Gizli Defteri-9. kitabıdır.</p>"
+    },
+    {
+        "itemName":"Çalıkuşu",
+        "itemAuthor":"Reşat Nuri",
+        "itemImage":"image/image4.jpg",
+        "itemCreationDate":"1939",
+        "itemDescription":"<p>Çalıkuşu ilk kez 1922 yılında Vakit gazetesinde tefrika edilmiş ve aynı yıl kitap olarak basılmıştır. Beşinci baskısından sonra eser, 1939 yılında bizzat Reşat Nuri Güntekin tarafından sonra tekrar yayımlanmıştır. Bu kitap söz konusu baskısından yararlanılarak aslına uygun olarak yayına hazırlanmıştır.</p>"
+    },
+
+    {
+        "itemName":"Çevrimiçi Kız",
+        "itemAuthor":"Zoe Sugg",
+        "itemImage":"image/image5.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>Twitter’da 3,5 milyon, Instagram’da 4,5 milyon ve Youtube’da 8 milyon takipçiye sahip internet fenomeni ve Zoella blogunun yaratıcısı Zoe Sugg’ın ilk romanı. Hakları 35 ülkeye satıldı ve sadece İngiltere’de 400.000’in üzerinde satışa ulaşarak 2014’ün en hızlı satan kitabı oldu. Yayımlandığı ilk hafta satış rekoru kırarak E. L. James, J. K. Rowling, Stephenie Meyer ve Dan Brown gibi yazarları geride bıraktı. Penny’nin bir sırrı var. Penny, Çevrimiçi Kız rumuzuyla arkadaşlık, erkekler, çılgın ailesi ve hayatını ele geçirmeye başlayan panik ataklarla ilgili hislerini blogunda paylaşmaktadır. İşler iyice sarpa sarınca, ailesi hava değişiminin Penny’ye iyi geleceğini düşünerek onu hemen New York’a götürür. </p>"
+    },
+    
+    {
+        "itemName": "Da Vinci Şifresi",
+        "itemAuthor": "Dan Brown",
+        "itemimage": "image/image6.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<pSion Tarikatı - 1099 yılında kurulmuş olan gizil Avrupa cemiyeti - gerçek bir topluluktur. 1975 yılında Parisin Milli Kütüphanesi, Sir Isaac Newton, Botticelli, Victor Hugo ve Leonardo da Vinci de dahil olmak üzere, Sion Tarikatının sayısız üyelerinin isimlerini içeren, Les Dossiers Secrets diye bilinen parşömenleri ortaya çıkarmıştı Bu romanda bahsi geçen tüm sanat eserleri, mimari yapılar, belgeler ve gizli ayinler gerçektir.</p>",
+    
+    },
+   
+    
+];
